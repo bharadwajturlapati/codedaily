c7e6613e66c6bc52414029b0ff32b82e6eabe4fe
bharadwaj.turlapati <tbj.name@gmail.com>
2018-09-20 08:20:48 +0530

    buggy code, algorithm fails with null pointer need to fix in next commit adding tet cases.

---
+++ codesamples/src/main/java/binarytrees/Node.java
@@ -0,0 +1,11 @@
+package binarytrees;
+
+public class Node {
+	int val;
+	public Node left;
+	public Node right;
+
+	public Node(int data) {
+		this.val = data;
+	}
+}
--- codesamples/src/main/java/practisebinarytree/BinaryTreeSolution.java
+++ codesamples/src/main/java/practisebinarytree/BinaryTreeSolution.java
@@ -1,5 +1,8 @@
 package practisebinarytree;
 
+import binarytrees.BinaryTree;
+import binarytrees.Node;
+
 public class BinaryTreeSolution {
 
 	public static void main(String[] args) {
@@ -11,40 +14,4 @@ public class BinaryTreeSolution {
 		System.out.println(tree.maxsumPath(tree.root));
 	}
 
-}
-
-class BinaryTree {
-	Node root;
-
-	public void inordertraversal(Node node) {
-		if (node != null) {
-			inordertraversal(node.left);
-			System.out.println(node.val);
-			inordertraversal(node.right);
-		}
-	}
-
-	public int maxsumPath(Node node) {
-		/*if (node == null) {
-			return 0;
-		}*/
-		if(node.left == null && node.right == null){
-			return node.val;
-		}
-		int left = maxsumPath(node.left);
-		int right = maxsumPath(node.right);
-		int maxofleftright = left > right ? left : right;
-		int maxofnodewithleftright = maxofleftright + node.val > node.val ? maxofleftright + node.val : node.val;
-		return maxofnodewithleftright;
-	}
-}
-
-class Node {
-	int val;
-	Node left;
-	Node right;
-
-	public Node(int data) {
-		this.val = data;
-	}
 }
