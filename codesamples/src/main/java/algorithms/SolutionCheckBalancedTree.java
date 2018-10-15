ed05de59582316e1e8b05acdf534dab848de31cf
bharadwaj.turlapati <tbj.name@gmail.com>
2018-10-16 01:58:37 +0530

    be careful while writing such code

--- codesamples/src/main/java/algorithms/SolutionCheckBalancedTree.java
+++ codesamples/src/main/java/algorithms/SolutionCheckBalancedTree.java
@@ -11,7 +11,10 @@ public class SolutionCheckBalancedTree {
 		if(root == null){
 			return true;
 		}
-		if((min != null && root.val<=min) ||(max !=null && root.val> max)){
+		// hacker earth needs this fix for the input 7
+		// which is value of the current node
+		// cannot be greater than equal to max
+		if((min != null && root.val<=min) ||(max !=null && root.val>= max)){
 			return false;
 		}
 		if(!checkBST(root.left, min, root.val) || !checkBST(root.right, root.val,max)){
