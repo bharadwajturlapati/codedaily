630faa5309e1ca66948ec709595dc58255528734
bharadwajturlapati <tbj.name@gmail.com>
2018-10-20 23:48:32 +0530

    introducing hackerrank meta class every hackerrank problem can be classified into this

---
+++ codesamples/src/main/java/HackerRank/HackerRankMeta.java
@@ -0,0 +1,12 @@
+package HackerRank;
+
+public  abstract class HackerRankMeta {
+	
+	private String url = "";
+	
+	
+	public String getURL() {
+		return this.url;
+	}
+
+}
---
+++ codesamples/src/main/java/Sorting/Comparator.java
@@ -0,0 +1,17 @@
+package Sorting;
+
+import HackerRank.HackerRankMeta;
+
+/* URL
+ * PracticeInterview Preparation KitSortingSorting: Comparator
+ */
+public class Comparator extends HackerRankMeta {
+	
+	
+	@Override
+	public String getURL() {
+		String url = "https://www.hackerrank.com/interview/interview-preparation-kit/sorting/challenges";
+		return url;
+	}
+
+}
